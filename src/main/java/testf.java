2679abdf6e59f24d1754fccdc13bb53ea1ab9d64
Babayevrustam <Babayevrustam@github.com>
2023-03-07 00:42:26 +0400

    first lesson s

---
+++ src/main/java/testf.java
@@ -0,0 +1,13 @@
+public class testf {
+    public static void main(String[] args) {
+    int a = 6;
+    int b = 4;
+        System.out.println(a == b);
+        System.out.println(a !=  b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+    }
+}
+
